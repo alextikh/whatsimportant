6ffb9d9ac71290937859acb92acbb27209fc9ef9
alextikh <alex.tichonenkov@gmail.com>
2016-09-22 12:24:59 +0300

    added total count of top member messages

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from re import compile
 from datetime import datetime, timedelta
-
+from operator import itemgetter
+from itertools import islice
 from os import remove, listdir
+from re import compile
 
 filename = 'chat.txt'
 cooldown_period = timedelta(hours=1)
 num_top_members = 5
 
+
 def parse_message(message):
     r = compile('(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<year>\d{2}), (?P<hour>\d{2}):(?P<minutes>\d{2}) - '
                 '(?P<sender>[^:]+): (?P<content>.+)')
     match = r.match(message)
-    if match != None:
+    if match is not None:
         return {'year': int(match.group('year')),
                 'month': int(match.group('month')),
                 'day': int(match.group('day')),
@@ -30,7 +32,7 @@ def split_conversations(messages):
     for msg in messages[1:]:
         msg_data1 = parse_message(base_msg)
         msg_data2 = parse_message(msg)
-        if msg_data2 != None:
+        if msg_data2 is not None:
             msg_d1 = datetime(year=msg_data1['year'], month=msg_data1['month'], day=msg_data1['day'],
                               hour=msg_data1['hour'], minute=msg_data1['minutes'])
             msg_d2 = datetime(year=msg_data2['year'], month=msg_data2['month'], day=msg_data2['day'],
@@ -49,12 +51,17 @@ def find_top_members(messages):
     members = {}
     for msg in messages:
         msg_data = parse_message(msg)
-        sender = msg_data['sender']
-        if sender in members:
-            members[sender] += 1
-        else:
-            members[sender] = 1
-    members = sorted(members.items(), key=lambda x: x[])
+        if msg_data is not None:
+            sender = msg_data['sender']
+            if sender in members:
+                members[sender] += 1
+            else:
+                members[sender] = 1
+    members = sorted(members.items(), key=itemgetter(1), reverse=True)
+    if len(members) <= num_top_members:
+        return dict(members)
+    else:
+        return dict(members[:num_top_members])
 
 
 def log(conversations):
@@ -62,7 +69,7 @@ def log(conversations):
 
     r = compile('conversation\d{1,}.txt')
     for file in listdir('.'):
-        if r.match(file) != None:
+        if r.match(file) is not None:
             remove(file)
 
     # create new logs
@@ -73,11 +80,13 @@ def log(conversations):
         with open(logfilename, 'w', encoding='utf-8') as logfile:
             logfile.writelines(conversation)
 
+
 def main():
     with open(filename, 'r', encoding='utf8') as chatfile:
         messages = chatfile.readlines()
-        conversations = split_conversations(messages)
-        log(conversations)
+        print(find_top_members(messages))
+        # conversations = split_conversations(messages)
+        # log(conversations)
 
 
 if __name__ == '__main__':
